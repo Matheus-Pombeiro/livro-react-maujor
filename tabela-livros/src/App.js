90ecdfc854a62788f40cf17e4f4ea0d0b869d5ed
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-01-11 21:35:24 -0300

    ADD: state and props

--- tabela-livros/src/App.js
+++ tabela-livros/src/App.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import TabelaHead from "./components/TabelaHead";
 import TabelaFoot from "./components/TabelaFoot";
 import TabelaBody from "./components/TabelaBody";
 
-function App() {
-  return (
-    <table className="tabela">
-      <TabelaHead />
-      <TabelaFoot />
-      <TabelaBody />
-    </table>
-  )
+class App extends Component {
+  state = {
+    livros: [
+      {
+        id: "978-85-7522-632-2",
+        titulo: "CSS Grid Layout",
+        autor: "Mauricio Samy Silva"
+      },
+      {
+        id: "978-85-7522-677-3",
+        titulo: "Node Essencial",
+        autor: "Ricardo R. Lecheta"
+      },
+      {
+        id: "978-85-7522-512-7",
+        titulo: "Aprendendo Material Design",
+        autor: "Kyle Mew"
+      }
+    ]
+  };
+  render() {
+    return (
+      <table className="tabela">
+        <TabelaHead />
+        <TabelaFoot />
+        <TabelaBody livros={ this.state.livros } />
+      </table>
+    );
+  }
 }
 
 export default App;
